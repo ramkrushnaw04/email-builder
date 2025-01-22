a3aad77dbef43ac0949dc0069666492b5d26affc
ramkrushnaw04 <ramkrushnaw04@gmail.com>
2025-01-22 12:17:24 +0530

    final build

--- frontend/src/components/Template.jsx
+++ frontend/src/components/Template.jsx
@@ -29,18 +29,18 @@ const Template = ({ name }) => {
 
     return (
         <button
-            className="max-h-[400px] overflow-hidden max-w-[300px] m-auto bg-gray-100 hover:bg-gray-200 rounded-md shadow-md relative transition-all duration-300 ease-in-out border border-gray-300 group"
+            className="h-[400px] overflow-hidden w-[300px] m-auto bg-gray-100 hover:bg-gray-200 rounded-md shadow-md relative transition-all duration-300 ease-in-out border border-gray-300 group"
             onClick={navigateTo}
         >
             {/* Main Content */}
             <div
-                className="overflow-hidden rounded-md pointer-events-none group-hover:opacity-50 transition-opacity duration-300"
-                dangerouslySetInnerHTML={{ __html: htmlContent }}
+                className="flex items-center justify-center h-full w-full text-gray-700 text-lg font-medium pointer-events-none group-hover:opacity-50 transition-opacity duration-300"
             >
+                {name}
             </div>
 
             {/* Poster */}
-            <div className="absolute inset-0 hidden group-hover:flex flex-col justify-center items-center bg-gray-900 bg-opacity-80 text-white text-lg font-medium rounded-md">
+            <div className="absolute w-full h-full inset-0 hidden group-hover:flex flex-col justify-center items-center bg-gray-900 bg-opacity-80 text-white text-lg font-medium rounded-md">
                 <span>{name}</span>
                 <span className="text-sm">Start Editing...</span>
             </div>
